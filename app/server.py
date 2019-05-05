575c0ba4406c4776f0d7e190cc7d98287c8c086a
kori73 <koray.beyaz73@gmail.com>
2019-05-05 16:32:42 +0300

    customize for vehicle types model

--- app/server.py
+++ app/server.py
@@ -8,11 +8,11 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-# export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
+# export_file_url = 'https://drive.google.com/uc?export=download&id=1q_kz7XBSi0HX8WTvG-_KSPla8Cv-SU0i'
 export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
-export_file_name = 'export.pkl'
+export_file_name = 'vehicle_types.pkl'
 
-classes = ['black', 'grizzly', 'teddys']
+classes = ['convertible', 'hatchback', 'sedan', 'suv', 'van']
 path = Path(__file__).parent
 
 app = Starlette()

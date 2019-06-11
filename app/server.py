a16a8dc2276d2a2a7834141c200c003527305e3f
koray.beyaz73@gmail.com <koray.beyaz73@gmail.com>
2019-06-11 18:13:55 +0300

    catch up with master

--- app/server.py
+++ app/server.py
@@ -1,12 +1,13 @@
-from starlette.applications import Starlette
-from starlette.responses import HTMLResponse, JSONResponse
-from starlette.staticfiles import StaticFiles
-from starlette.middleware.cors import CORSMiddleware
-import uvicorn, aiohttp, asyncio
-from io import BytesIO
-
+import aiohttp
+import asyncio
+import uvicorn
 from fastai import *
 from fastai.vision import *
+from io import BytesIO
+from starlette.applications import Starlette
+from starlette.middleware.cors import CORSMiddleware
+from starlette.responses import HTMLResponse, JSONResponse
+from starlette.staticfiles import StaticFiles
 
 # Model URL and name
 export_file_url = 'https://drive.google.com/uc?export=download&id=1q_kz7XBSi0HX8WTvG-_KSPla8Cv-SU0i'
@@ -16,15 +17,20 @@ classes = ['convertible', 'hatchback', 'sedan', 'suv', 'van']
 path = Path(__file__).parent
 
 app = Starlette()
-app.add_middleware(CORSMiddleware, allow_origins=['*'], allow_headers=['X-Requested-With', 'Content-Type'])
+app.add_middleware(CORSMiddleware, allow_origins=[
+                   '*'], allow_headers=['X-Requested-With', 'Content-Type'])
 app.mount('/static', StaticFiles(directory='app/static'))
 
+
 async def download_file(url, dest):
-    if dest.exists(): return
+    if dest.exists():
+        return
     async with aiohttp.ClientSession() as session:
         async with session.get(url) as response:
             data = await response.read()
-            with open(dest, 'wb') as f: f.write(data)
+            with open(dest, 'wb') as f:
+                f.write(data)
+
 
 async def setup_learner():
     await download_file(export_file_url, path/export_file_name)
@@ -44,10 +50,12 @@ tasks = [asyncio.ensure_future(setup_learner())]
 learn = loop.run_until_complete(asyncio.gather(*tasks))[0]
 loop.close()
 
+
 @app.route('/')
 def index(request):
     html = path/'view'/'index.html'
     return HTMLResponse(html.open().read())
+
 
 @app.route('/analyze', methods=['POST'])
 async def analyze(request):
@@ -58,4 +66,5 @@ async def analyze(request):
     return JSONResponse({'result': str(prediction)})
 
 if __name__ == '__main__':
-    if 'serve' in sys.argv: uvicorn.run(app=app, host='0.0.0.0', port=5042)
+    if 'serve' in sys.argv:
+        uvicorn.run(app=app, host='0.0.0.0', port=5042)
